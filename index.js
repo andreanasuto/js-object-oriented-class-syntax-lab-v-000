b78148bc0c644498202b429d9ad6d0c717720ec8
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-03 21:16:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class BoardMember {
   }
 
   sayHi() {
-    console.log(Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.);
+    console.log(`Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`);
     return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
   }
 
